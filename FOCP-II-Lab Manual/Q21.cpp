10e212862ee5fa96642a1c9ae7b0a80468bac7b2
Chehak <chehakarora08@gmail.com>
2026-01-26 00:48:14 +0530

    Create Q21.cpp

---
+++ FOCP-II-Lab Manual/Q21.cpp
@@ -0,0 +1,25 @@
+//Program to display a rectangular hollow frame.
+#include <iostream>
+
+int main() {
+    int rows, columns;
+
+    std::cout<<"Enter number of rows: ";
+    std::cin>>rows;
+    std::cout<<"Enter number of columns: ";
+    std::cin>>columns;
+
+    for(int i=1;i<=rows;i++) {
+        for(int j=1;j<=columns;j++) {
+            if(i==1 || i==rows || j==1 || j==columns) {
+                std::cout<<"*";
+            } 
+            else {
+                std::cout<<" ";
+            }
+        }
+        std::cout<<"\n";
+    }
+
+    return 0;
+}
