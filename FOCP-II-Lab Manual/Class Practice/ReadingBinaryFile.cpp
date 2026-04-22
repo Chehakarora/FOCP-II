8ca64a9e8919ad28742a257731df4367e95e2c70
Chehak <chehakarora08@gmail.com>
2026-04-22 10:08:09 +0530

    Create ReadingBinaryFile.cpp

---
+++ FOCP-II-Lab Manual/Class Practice/ReadingBinaryFile.cpp
@@ -0,0 +1,24 @@
+//Reading Binary File
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+struct Student {
+    int id;
+    char name[20];
+    float marks;
+};
+
+int main(){
+    Student s;
+    ifstream fin("student.dat", ios::binary);
+    if(!fin){
+        cout<<"File not found!";
+        return 1;
+    }
+    fin.read((char*)&s, sizeof(s));
+    fin.close();
+    cout<<"ID: "<<s.id<<endl;
+    cout<<"Name: "<<s.name<<endl;
+    cout<<"Marks: "<<s.marks<<endl;
+}
